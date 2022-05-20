d2d8fa2033cb54b52e3b870721b8083289661a38
Jetson Nano <myz92524@hotmail.com>
2022-05-20 15:00:46 +0900

    Add auto_set_navigating_target function. Has't  been finished yet.

--- build/behavior_tree/catkin_generated/installspace/behavior_treeConfig.cmake
+++ build/behavior_tree/catkin_generated/installspace/behavior_treeConfig.cmake
@@ -185,7 +185,7 @@ foreach(t ${behavior_tree_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "roscpp;rospy;std_msgs;message_runtime")
+set(depends "roscpp;rospy;std_msgs;message_runtime;move_base_msgs;actionlib")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
