aacda696f4ad82233eb31421a28284b0b7667499
Mou Yingzhou <myz92524@hotmail.com>
2022-08-26 13:22:42 +0900

    add yolo package

---
+++ src/ros_yolo/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/lostm/文档/machine_learning/ros_yolo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/lostm/文档/machine_learning/ros_yolo/devel;/home/lostm/文档/arduino_ws/devel;/opt/ros/noetic'.split(';') if '/home/lostm/文档/machine_learning/ros_yolo/devel;/home/lostm/文档/arduino_ws/devel;/opt/ros/noetic' != '' else []
