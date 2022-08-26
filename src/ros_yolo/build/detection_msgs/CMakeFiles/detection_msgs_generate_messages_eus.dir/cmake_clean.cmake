2361a8cbfd64eff8654f8de6fbfafbfb4363d5df
Mou Yingzhou <myz92524@hotmail.com>
2022-08-26 12:37:43 +0900

    add yolo package

---
+++ src/ros_yolo/build/detection_msgs/CMakeFiles/detection_msgs_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/lostm/文档/machine_learning/ros_yolo/devel/share/roseus/ros/detection_msgs/manifest.l"
+  "/home/lostm/文档/machine_learning/ros_yolo/devel/share/roseus/ros/detection_msgs/msg/BoundingBox.l"
+  "/home/lostm/文档/machine_learning/ros_yolo/devel/share/roseus/ros/detection_msgs/msg/BoundingBoxes.l"
+  "CMakeFiles/detection_msgs_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/detection_msgs_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/ros_yolo/devel/share/detection_msgs/cmake/detection_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(detection_msgs_MSG_INCLUDE_DIRS "/home/lostm/文档/machine_learning/ros_yolo/src/detection_msgs/msg")
+set(detection_msgs_MSG_DEPENDENCIES std_msgs)
